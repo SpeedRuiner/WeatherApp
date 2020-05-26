856b3523c500c800277f43e114df5b74fdd1aa82
Piter <simpel-box@yandex.ru>
2020-05-26 07:24:39 +0300

    Add send values in new Activity

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -5,17 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editTextFindCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextFindCity = findViewById(R.id.EditTextfindCity);
     }
 
     public void onClickWeatherForecast(View view) {
-        Intent weatherIntent = new Intent(this, WeatherForecastActivity.class);
-        startActivity(weatherIntent);
+        String nameCity = editTextFindCity.getText().toString().trim();
+        if (!nameCity.isEmpty()){
+            Intent weatherIntent = new Intent(this, WeatherForecastActivity.class);
+            weatherIntent.putExtra("name", nameCity);
+            startActivity(weatherIntent);
+        } else {
+            Toast.makeText(this, R.string.enter_city, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/com/example/weather/WeatherForecastActivity.java
+++ app/src/main/java/com/example/weather/WeatherForecastActivity.java
@@ -2,13 +2,27 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WeatherForecastActivity extends AppCompatActivity {
+
+    private TextView textViewValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_forecast);
+        textViewValues = findViewById(R.id.textViewValues);
+        Intent weatherIntent = getIntent();
+        if (weatherIntent.hasExtra("name")){
+            String name = weatherIntent.getStringExtra("name");
+            textViewValues.setText(name);
+        } else {
+            Intent backToMain = new Intent(this, MainActivity.class);
+            startActivity(backToMain);
+        }
+
     }
 }
